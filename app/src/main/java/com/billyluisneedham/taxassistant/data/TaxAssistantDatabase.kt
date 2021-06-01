d51f2267ad0dd9f000e48057bdf49c4b88b1a0a2
BillyLuisNeedham <billyluisneedham@gmail.com>
2021-06-01 20:40:29 +0100

    db refactor

--- app/src/main/java/com/billyluisneedham/taxassistant/data/TaxAssistantDatabase.kt
+++ app/src/main/java/com/billyluisneedham/taxassistant/data/TaxAssistantDatabase.kt
@@ -1,4 +1,4 @@
-package com.billyluisneedham.taxassistant.database
+package com.billyluisneedham.taxassistant.data
 
 import android.content.Context
 import androidx.room.Database
@@ -8,23 +8,24 @@ import com.billyluisneedham.taxassistant.expenses.Expense
 import com.billyluisneedham.taxassistant.expenses.ExpenseDao
 
 @Database(entities = [Expense::class], version = 1)
-abstract class AppDatabase : RoomDatabase() {
+abstract class TaxAssistantDatabase : RoomDatabase() {
     abstract fun expensesDao(): ExpenseDao
 
     companion object {
-        private const val DATABASE_NAME = "AppDatabase"
+        private const val DATABASE_NAME = "Tax-Assistant-Database.db"
 
         @Volatile
-        private var instance: AppDatabase? = null
+        private var instance: TaxAssistantDatabase? = null
 
-        fun getInstance(context: Context): AppDatabase {
+        fun getInstance(context: Context): TaxAssistantDatabase {
             return instance ?: synchronized(this) {
                 instance ?: buildDatabase(context).also { instance = it }
             }
         }
 
-        private fun buildDatabase(context: Context): AppDatabase {
-            return Room.databaseBuilder(context, AppDatabase::class.java, DATABASE_NAME)
+        private fun buildDatabase(context: Context): TaxAssistantDatabase {
+            return Room.databaseBuilder(context, TaxAssistantDatabase::class.java, DATABASE_NAME)
+                .fallbackToDestructiveMigration()
                 .build()
         }
     }
