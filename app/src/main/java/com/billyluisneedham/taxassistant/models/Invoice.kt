c9239a5dab761200ff041b2b4dac75bc92152de8
BillyLuisNeedham <billyluisneedham@gmail.com>
2021-06-01 19:36:45 +0100

    invoice coloumn data added

--- app/src/main/java/com/billyluisneedham/taxassistant/models/Invoice.kt
+++ app/src/main/java/com/billyluisneedham/taxassistant/models/Invoice.kt
@@ -1,15 +1,18 @@
 package com.billyluisneedham.taxassistant.models
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "invoices")
 data class Invoice(
-    val invoiceId: Long,
-    val refNumber: String,
-    val whoHasBeenInvoiced: String,
-    val invoiceDateTimeStamp: Long,
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "invoice_id") val invoiceId: Long,
+    @ColumnInfo(name = "ref_number") val refNumber: String,
+    @ColumnInfo(name = "name_of_person_invoiced") val nameOfPersonInvoiced: String,
+    @ColumnInfo(name = "invoice_date_time_stamp") val invoiceDateTimeStamp: Long,
     val amount: Double,
-    val hasBeenPaid: Boolean = false,
-    val invoiceUrl: String,
+    @ColumnInfo(name = "has_been_paid") val hasBeenPaid: Boolean = false,
+    @ColumnInfo(name = "invoice_url") val invoiceUrl: String,
     val notes: String,
 )
