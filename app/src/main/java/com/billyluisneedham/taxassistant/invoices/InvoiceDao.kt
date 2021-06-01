66395159d7e832eb877b11c7bf54b584f01d6438
BillyLuisNeedham <billyluisneedham@gmail.com>
2021-06-01 20:59:10 +0100

    invoice dao build out

--- app/src/main/java/com/billyluisneedham/taxassistant/invoices/InvoiceDao.kt
+++ app/src/main/java/com/billyluisneedham/taxassistant/invoices/InvoiceDao.kt
@@ -1,7 +1,12 @@
 package com.billyluisneedham.taxassistant.invoices
 
+import androidx.room.Query
 import com.billyluisneedham.taxassistant.data.BaseDao
 
 interface InvoiceDao: BaseDao<Invoice> {
+    @Query("SELECT * from invoices")
+    fun getAll(): List<Invoice>
 
+    @Query("DELETE from invoices")
+    fun deleteAll()
 }
