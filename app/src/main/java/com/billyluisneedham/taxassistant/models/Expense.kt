6d1323ec960710c22a241c825779302eb0e13303
BillyLuisNeedham <billyluisneedham@gmail.com>
2021-06-01 19:30:31 +0100

    expense model built out

---
+++ app/src/main/java/com/billyluisneedham/taxassistant/models/Expense.kt
@@ -0,0 +1,16 @@
+package com.billyluisneedham.taxassistant.models
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "expenses")
+data class Expense(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "expense_id") val expenseId: Long,
+    val name: String,
+    @ColumnInfo(name = "date_time_stamp") val dateTimeStamp: Long,
+    val amount: Double,
+    @ColumnInfo(name = "receipt_url") val receiptUrl: String,
+    val notes: String
+)
