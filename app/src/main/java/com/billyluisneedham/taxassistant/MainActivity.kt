6698a40b3020a58d7e416774ca87d7e6ad87296a
BillyLuisNeedham <billyluisneedham@gmail.com>
2021-06-28 17:19:10 +0100

    converted Main into dagger

--- app/src/main/java/com/billyluisneedham/taxassistant/MainActivity.kt
+++ app/src/main/java/com/billyluisneedham/taxassistant/MainActivity.kt
@@ -1,15 +1,15 @@
 package com.billyluisneedham.taxassistant
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.billyluisneedham.taxassistant.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import dagger.android.support.DaggerAppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : DaggerAppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
