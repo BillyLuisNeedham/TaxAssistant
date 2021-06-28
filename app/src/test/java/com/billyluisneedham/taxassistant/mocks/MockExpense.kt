cf4d9e6cbad97c8ba7ef0c3c3f53998bc23aa3e1
BillyLuisNeedham <billyluisneedham@gmail.com>
2021-06-28 16:51:57 +0100

    MockExpense and MockInvoice added to unit tests

---
+++ app/src/test/java/com/billyluisneedham/taxassistant/mocks/MockExpense.kt
@@ -0,0 +1,16 @@
+package com.billyluisneedham.taxassistant.testutils.mocks
+
+import com.billyluisneedham.taxassistant.expenses.Expense
+
+class MockExpenseUnit {
+    companion object {
+        val expense = Expense(
+            expenseId = 1,
+            name = "test expense",
+            dateTimeStamp = 12345,
+            amount = 58.33,
+            receiptUrl = "www.test.com",
+            notes = "test notes"
+        )
+    }
+}
